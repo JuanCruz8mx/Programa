a1caff3cf10ce2016dc6f7b0119a1d53b05faef8
Efrain-delgado <delgadopintorefrain@gmail.com>
2025-06-16 00:54:50 -0600

    Add files via upload
    base de datos de centros de distribucion de una empresa

---
+++ cedis.py
@@ -0,0 +1,48 @@
+import json
+cedis={
+    "metropolitano en azcapotzalco":"ciudad de mexico",
+    "tijuana":"baja california",
+    "mexicali":"baja california",
+    "hermosillo":"sonora",
+    "cd juarez/cd obregon":"sonora",
+    "monterrey":"nuevo leon",
+    "saltillo":"coahuila",
+    "torreon":"coahuila",
+    "reyona":"tamaulipas",
+    "matamoros":"tamaulipas",
+    "cd victoria":"tamaulipas",
+    "altamira":"tamaulipas",
+    "ciudad mante":"tamaulipas",
+    "guadalajara":"jalisco",
+    "leon":"guanajuato",
+    "aguas calientes":"aguas calientes",
+    "colima":"colima",
+    "morelia":"michoacan",
+    "puebla":"puebla",
+    "queretaro":"queretaro",
+    "pachuca":"hidalgo",
+    "toluca":"estado de mexico",
+    "cuernavaca":"morelos",
+    "ciudad de mexico":"CDMX",
+    "acapulco":"guerrero",
+    "chipalcingo":"guerrero",
+    "oaxaca":"oaxaca",
+    "tapachula":"chiapas",
+    "veracruz":"veracruz",
+    "villa hermosa":"tabasco",
+    "coatzacoalcos":"veracruz",
+    "merida":"yucatan",
+    "cancun":"quintana roo",
+    "chetumal":"quintana roo"
+
+    
+
+}
+
+nombre_de_archivo="centros de distribucion BIMBO en México"
+try:
+    with open(nombre_de_archivo,'w', encoding='utf-8') as archivo_json:
+        json.dump(cedis, archivo_json, indent=4, ensure_ascii=False)
+        print(f"'{nombre_de_archivo}' creado exitosamente.")
+except IOError as e:
+    print(f'error al escribir en el archivo: {e}')
