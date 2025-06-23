c0d1584144c070ca42aa74dc756266c64341c5df
JuanCruz <juancruzosorio.mx@gmail.com>
2025-06-23 16:54:20 -0600

    Program

--- Programa/main.py
+++ Programa/main.py
@@ -6,20 +6,20 @@ class main():
     def __init__(self):
         self.G=Nx.Graph()
         self.v=Tk.Tk()
-        self.v.geometry('1500x800+200+100')
+        self.v.geometry('1300x650+300+200')
         self.v.title('RouteLigic')
-        self.v.config(bg="skyblue")
+        self.v.config(bg='#404040')
         self.nodos=set()         # Cojunto para evitar nodos duplicados
         self.csv()
         self.Grafo()
         self.ventana()
         self.v.mainloop()
     def ventana(self):
-        frame=Tk.Frame(self.v); frame.grid(row=0, column=0,sticky='N',padx=1,pady=1)
+        frame=Tk.Frame(self.v, bg='#404040'); frame.grid(row=0, column=0,sticky='N',padx=1,pady=1)
         frame2=Tk.Frame(self.v); frame2.grid(row=1, column=0, columnspan=2, sticky='S',padx=1,pady=1)
         self.frame3=Tk.Frame(self.v); self.frame3.grid(row=0, column=2, rowspan=3, columnspan=4, sticky='NE',padx=5,pady=5)
 
-        self.canvas = Tk.Canvas(self.frame3, width=1200, height=700, bg='white')
+        self.canvas = Tk.Canvas(self.frame3, width=800, height=600, bg='#f8f8f8')
         self.scroll_x = Tk.Scrollbar(self.frame3, orient='horizontal', command=self.canvas.xview)
         self.scroll_y = Tk.Scrollbar(self.frame3, orient='vertical', command=self.canvas.yview)
         self.canvas.configure(xscrollcommand=self.scroll_x.set, yscrollcommand=self.scroll_y.set)
@@ -30,57 +30,56 @@ class main():
 
         opciones = sorted(self.nodos)   # Ordena los nodos en una lista ordenada
 
-        origen=Tk.Label(frame, text='Origen: ', font=('arial', 12)); origen.grid(row=0, column=1, padx=2, pady=2, sticky='E')
+        origen=Tk.Label(frame, text='Origen: ', font=('arial', 15), bg='#404040'); origen.grid(row=0, column=1, padx=2, pady=2, sticky='E')
         origen_var = Tk.StringVar()     # Lo guarda para poder usar iterar despues
         origen_var.set(opciones[0])
         i_name = Tk.OptionMenu(frame, origen_var, *opciones)
-        i_name.config(font=('arial',12), width=15); i_name.grid(row=0, column=2, padx=2, pady=2, sticky='NSEW')
+        i_name.config(font=('arial',15), width=15, bg='#722F37'); i_name.grid(row=0, column=2, padx=2, pady=2, sticky='NSEW')
         
-        destino=Tk.Label(frame, text='Destino: ', font=('arial', 12)); destino.grid(row=1, column=1, padx=2, pady=2,sticky='E')
+        destino=Tk.Label(frame, text='Destino: ', font=('arial', 15), bg='#404040'); destino.grid(row=1, column=1, padx=2, pady=2,sticky='E')
         destino_var = Tk.StringVar()
         destino_var.set(opciones[0])
         i2_name = Tk.OptionMenu(frame, destino_var, *opciones)
-        i2_name.config(font=('arial',12), width=15); i2_name.grid(row=1, column=2, padx=2, pady=2, sticky='NSEW')
+        i2_name.config(font=('arial',15), width=15, bg='#722F37'); i2_name.grid(row=1, column=2, padx=2, pady=2, sticky='NSEW')
 
-        boton = Tk.Button(frame, text='Mejor ruta', font=('arial',12), bg='#0000FF', command=lambda: self.Grafo_ruta(origen_var.get(), destino_var.get())); boton.grid(row=3, column=2, padx=2, pady=2)
+        boton = Tk.Button(frame, text='Mejor ruta', font=('arial',15), bg='#722F37', command=lambda: self.Grafo_ruta(origen_var.get(), destino_var.get())); boton.grid(row=3, column=2, padx=2, pady=2)
 
-        self.respuesta=ScrolledText(frame2, width=50, height=29, wrap='word',font=('arial',12)); self.respuesta.grid(row=3,column=1, columnspan=3,padx=2,pady=2,sticky='NSEW')
+        self.respuesta=ScrolledText(frame2, width=40, height=20, wrap='word',font=('arial',15), bg='#f8f8f8'); self.respuesta.grid(row=3,column=1, columnspan=3,padx=2,pady=2,sticky='NSEW')
         self.mostrar_imagen('grafoCompleto.png')
 
     def mostrar_imagen(self, ruta):
         imagen_original = Image.open(ruta)
-        imagen_redimensionada = imagen_original.resize((1000, 700))
-        self.image = ImageTk.PhotoImage(imagen_redimensionada)
+        self.imagen = ImageTk.PhotoImage(imagen_original.resize((800, 600)))
         self.canvas.delete("all")  # Limpia el canvas anterior
-        self.canvas.create_image(0, 0, anchor='nw', image=self.image)
+        self.canvas.create_image(0, 0, anchor='nw', image=self.imagen)
         self.canvas.config(scrollregion=self.canvas.bbox('all'))
     def csv(self):
         self.G.clear()
         with open('centros.csv', 'r') as archivo:
             f=archivo.readlines()
             for i in f[1:]:     # split(','): Divide cada dato separados por comas y los guarda en una lista
-                N1, N2, Distancia, Capacidad, Tiempo=[x.strip() for x in i.split(',')] # Da el valor a cada uno de las variables
+                N1, N2, Distancia, Capacidad, Tiempo, self.Trasporte=[x.strip() for x in i.split(',')[:6]] # Da el valor a cada uno de las variables
                 #print(N1)
                 if N1 not in self.nodos:
-                    self.G.add_node(N1, distancia=Distancia, capacidad=Capacidad)    # Crea el N1
+                    self.G.add_node(N1, distancia=Distancia, capacidad=Capacidad, trasporte=self.Trasporte)    # Crea el N1
                     self.nodos.add(N1)
                 if N2 not in self.nodos:
-                    self.G.add_node(N2, distancia=Distancia, tiempo=Tiempo)      # Crea el N2
+                    self.G.add_node(N2, distancia=Distancia, tiempo=Tiempo,trasporte=self.Trasporte)      # Crea el N2
                     self.nodos.add(N2)
-                self.G.add_edge(N1, N2, distancia=float(Distancia), tiempo=float(Tiempo))     # Hace el grafo dirigido
+                self.G.add_edge(N1, N2, distancia=float(Distancia), tiempo=float(Tiempo), trasporte=str(self.Trasporte))     # Hace el grafo dirigido
                 self.pos=Nx.spring_layout(self.G, seed=20)    # Calcula las cordenadas y mantenerlas evitando que cambien
                 self.etiquetas={(origen, destino): f"{atributo['distancia']} km\n{atributo['tiempo']} h" for origen, destino, atributo in self.G.edges(data=True)}  # Obtiene etiquetas
     def Grafo(self):
-        Nx.draw(self.G, self.pos, with_labels=True, node_color='lightblue', edge_color='black', arrows=True)   # Diseño del grafo
-        Nx.draw_networkx_edge_labels(self.G, self.pos, edge_labels=self.etiquetas) # Ver las etiquetas en el grafo
+        Nx.draw(self.G, self.pos, with_labels=True, node_color='lightblue', edge_color='black',node_size=60, font_size=10)   # Diseño del grafo
+        #Nx.draw_networkx_edge_labels(self.G, self.pos, edge_labels=self.etiquetas, font_size=8) # Ver las etiquetas en el grafo
         plt.savefig('grafoCompleto.png')  # Guardar la imagen
         plt.close()
     def Ruta(self, ruta):
         r=list(zip(ruta[:-1], ruta[1:]))
-        Nx.draw(self.G, self.pos, with_labels=True, node_color='lightgray', edge_color='lightgray', arrows=True)  # Diseño de los nodes y aristas completo
-        Nx.draw_networkx_edges(self.G, self.pos, edgelist=r, edge_color='red', width=2.5, arrows=True)    # Diseño de los nodos de la ruta
+        Nx.draw(self.G, self.pos, with_labels=True, node_color='lightgray', edge_color='lightgray',node_size=60, font_size=10)  # Diseño de los nodes y aristas completo
+        Nx.draw_networkx_edges(self.G, self.pos, edgelist=r, edge_color='red', width=2, arrows=True)    # Diseño de los nodos de la ruta
         Nx.draw_networkx_nodes(self.G, self.pos, nodelist=ruta, node_color='red')     # Diseño de las aristas de la ruta
-        Nx.draw_networkx_edge_labels(self.G, self.pos, edge_labels=self.etiquetas)
+        #Nx.draw_networkx_edge_labels(self.G, self.pos, edge_labels=self.etiquetas, font_size=8)
         plt.savefig('grafoRuta.png')  # Guardar la imagen
         plt.close()
         self.mostrar_imagen('grafoRuta.png')
@@ -91,6 +90,7 @@ class main():
                 distancia_total=sum(self.G[a][b]['distancia'] for a, b in zip(ruta[:-1], ruta[1:]))
                 tiempo_total=sum(self.G[a][b]['tiempo'] for a, b in zip(ruta[:-1], ruta[1:]))
                 self.respuesta.insert('insert', f'Ruta más corta de {origen} a {destino}:\n {" → ".join(ruta)}\n')
+                self.respuesta.insert('insert', f'Tipo de trasporte: {self.Trasporte}\n')
                 self.respuesta.insert('insert', f'Distancia total: {distancia_total} km\n')
                 self.respuesta.insert('insert', f'Tiempo total: {tiempo_total} h\n\n')
                 self.Ruta(ruta)
